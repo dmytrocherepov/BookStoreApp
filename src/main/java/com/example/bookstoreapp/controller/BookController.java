ca6ba0b312ffb920c0831a1e861a512570e3281e
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-10-17 16:51:12 +0300

    added fixes after review

--- src/main/java/com/example/bookstoreapp/controller/BookController.java
+++ src/main/java/com/example/bookstoreapp/controller/BookController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/books")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
