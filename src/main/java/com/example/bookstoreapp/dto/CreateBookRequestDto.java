97a4b26dd9cde8565f861d537da8e453d99cdfd4
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-10-23 16:36:41 +0300

    added fixes

--- src/main/java/com/example/bookstoreapp/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstoreapp/dto/CreateBookRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.bookstoreapp.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
@@ -18,7 +19,7 @@ public record CreateBookRequestDto(
                 + "[- ]?[0-9X]$",
         message = "length must be 10 digits")
         String isbn,
-        @PositiveOrZero
+        @NotNull
         BigDecimal price,
         String description,
         String coverImage
