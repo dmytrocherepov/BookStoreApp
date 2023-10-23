03b40ebc6236224a94ee40a161c5359875a05089
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-10-23 16:38:47 +0300

    added fixes

--- src/main/java/com/example/bookstoreapp/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstoreapp/dto/CreateBookRequestDto.java
@@ -3,7 +3,6 @@ package com.example.bookstoreapp.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import org.hibernate.validator.constraints.Length;
 
