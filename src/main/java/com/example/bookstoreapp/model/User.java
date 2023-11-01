7d89cae126c5e2f02097451cda9a380da576c536
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-11-01 13:53:23 +0200

    checkstyle fixes

--- src/main/java/com/example/bookstoreapp/model/User.java
+++ src/main/java/com/example/bookstoreapp/model/User.java
@@ -2,7 +2,6 @@ package com.example.bookstoreapp.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
