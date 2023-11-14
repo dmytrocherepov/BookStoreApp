70556ba15c3a4fb7f4b31d9e21bd3cc5071039db
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-11-14 18:49:14 +0200

    checkstyle fixes

--- src/main/java/com/example/bookstoreapp/controller/AuthenticationController.java
+++ src/main/java/com/example/bookstoreapp/controller/AuthenticationController.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
