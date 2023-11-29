a3af5857eb3c97f4b09d30b7e95b09c1f1508444
Dmytro Cherepov <dmytro.cherepov.ukr@gmail.com>
2023-11-29 14:08:23 +0200

    new fix

--- src/main/java/com/example/bookstoreapp/controller/CategoryController.java
+++ src/main/java/com/example/bookstoreapp/controller/CategoryController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @Tag(name = "Category management ", description = "Endpoints for managing categories")
 @RestController
 @RequestMapping("/categories")
@@ -63,7 +65,7 @@ public class CategoryController {
     )
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public CategoryResponseDto getCategoryById(@PathVariable Long id) {
+    public CategoryResponseDto getCategoryById(@PathVariable @Positive Long id) {
         return categoryService.getById(id);
     }
 
